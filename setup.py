8b6f338a81a6616cc893bf09b700192b10dd15d9
Prateek Dutta <62097113+PrateekDutta2001@users.noreply.github.com>
2024-03-30 16:20:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='time-granularity',
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
     install_requires=[],  # Add dependencies if any
     author='Prateek Dutta',
     author_email='parteekdutta2001@email.com',
     description='A Python package for managing time granularity with options to re-execute at different intervals.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/PrateekDutta2001/time-granularity',
 )
